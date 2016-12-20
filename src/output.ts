fa2ab93e3f363c1bf9e35f522b376976b53a4a19
Ben Southgate <bsouthga@gmail.com>
2016-12-20 12:12:10 -0500

    feat: add generation options

--- src/output.ts
+++ src/output.ts
@@ -1,7 +1,7 @@
 import { Tyr } from 'tyranid';
 import * as fs from 'fs';
 import { Readable } from 'stream';
-import { generate } from './module';
+import { generate, InterfaceGenerationOptions } from './module';
 
 
 /**
@@ -11,10 +11,11 @@ import { generate } from './module';
  * 
  */
 export function generateStream(
-  collections: Tyr.CollectionInstance[]
+  collections: Tyr.CollectionInstance[],
+  opts?: InterfaceGenerationOptions
 ) {
   const stream = new Readable();
-  const td = generate(collections);
+  const td = generate(collections, opts);
   stream.push(td);
   stream.push(null);
   return stream;
@@ -29,9 +30,10 @@ export function generateStream(
  */
 export function generateFileSync(
   collections: Tyr.CollectionInstance[],
-  filename: string
+  filename: string,
+  opts?: InterfaceGenerationOptions
 ): string {
-  const td = generate(collections);
+  const td = generate(collections, opts);
   fs.writeFileSync(filename, td);
   return td;
 }
@@ -47,11 +49,17 @@ export function generateFileSync(
 export function generateFile(
   collections: Tyr.CollectionInstance[],
   filename: string,
+  opts?: InterfaceGenerationOptions | Function,
   cb?: Function
 ): Promise<string> {
+  if (opts instanceof Function) {
+    cb = opts;
+    opts = {};
+  }
+
   return new Promise((res, rej) => {
     try {
-      const td = generate(collections);
+      const td = generate(collections, opts);
       fs.writeFile(filename, td, (err) => {
         if (err) rej(err);
         if (cb) cb(err, td);
